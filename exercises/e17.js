7695a2007b33296856d49490b6ace9631fcad2f3
Ceech Hsu <ceechhsu@gmail.com>
2023-02-23 22:36:56 -0800

    passed test-17

--- exercises/e17.js
+++ exercises/e17.js
@@ -1,3 +1,19 @@
-export function minBy(array, cb) {}
+export function minBy(array, cb) {
+  let min = array[0];
+  for (const elem of array) {
+    if (cb(elem) < cb(min)) {
+      min = elem;
+    }
+  }
+  return min;
+}
 
-export function maxBy(array, cb) {}
+export function maxBy(array, cb) {
+  let max = array[0];
+  for (const elem of array) {
+    if (cb(elem) > cb(max)) {
+      max = elem;
+    }
+  }
+  return max;
+}
